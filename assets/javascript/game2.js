e56fd219d419ab444c0b16445b4679198721817c
Moogieee <Carie_ngo@yahoo.com>
2017-07-08 20:58:01 -0700

    Update game2.js

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,4 +1,5 @@
 
+// note to self: first mistake was setting the variables inside $(document).ready(function() {}); and my reset wouldn't work.
 
 // create a counter to keep track of the users total
 var counter = 0;
@@ -14,11 +15,11 @@ var crystal4 = 0;
 
 // function to reset the game
 function reset () {
-	alert('reset');
+// 	alert('reset');
 	targetNumber = Math.floor(Math.random() * 102 + 19);
-	console.log("targetNumber = " + targetNumber);
+// 	console.log("targetNumber = " + targetNumber);
  	crystal1 = Math.floor(Math.random() * 12 + 1);
-	console.log("c1 = " + crystal1);
+// 	console.log("c1 = " + crystal1);
 	crystal2 = Math.floor(Math.random() * 12 + 1);
 	crystal3 = Math.floor(Math.random() * 12 + 1);
 	crystal4 = Math.floor(Math.random() * 12 + 1);
@@ -46,11 +47,13 @@ targetNumber = Math.floor(Math.random() * 102 + 19);
 // set the number-to-guess header to match the targetNumber
 // eventually this will allow us to change the HTML to match the value in JavaScript.
 $("#number-to-guess").html(targetNumber);
+	
 //give the class ".crystal-img" to each crystal so that it can be CSSd
 $("#crystal1, #crystal2, #crystal3, #crystal4").addClass("crystal-image");
 
 reset();
-/*	
+	
+/* commented out the following lines of code because it got moved to the reset. I created a reset function so that I can call the function when needed. This hopefully makes things less repetitive.
 // give each crystal a random # from 1-12
 crystal1 = Math.floor(Math.random() * 12 + 1);
 console.log(crystal1);
@@ -70,6 +73,7 @@ $("#crystalValue").text('0');
 	$("#crystal3").attr("data-crystalvalue", crystal3);
 	$("#crystal4").attr("data-crystalvalue", crystal4);
 */
+	
 // each crystal image (with all its classes and attributes) will get added to the page.
 	$("#crystal1").append(crystal1);
 	$("#crystal2").append(crystal2);
@@ -85,8 +89,9 @@ $("#crystal1, #crystal2, #crystal3, #crystal4").on("click", function () {
 	// since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
 	var crystalValue = ($(this).attr("data-crystalvalue"));
-	crystalValue = parseInt(crystalValue); // when you store the click it becomes a string. parseInt the string so that it becomes a number and can be incremented.
-
+	// when you store the click it becomes a string. parseInt the string so that it becomes a number and can be incremented.
+	crystalValue = parseInt(crystalValue); 
+	
 // we then add the crystalValue to the users "counter" which is a global variable
 // every click, from every crystal adds to the global counter.
 	counter += crystalValue;
