e20facbe43480054fd5eadafe4255573c5d59834
unknown <moogieee@ODSPR10234W45.odsdai.netdai.com>
2017-07-11 13:07:57 -0700

    got reset function to work, made crystal images responsive

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,79 +1,59 @@
+$(document).ready(function() {
 
-// note to self: first mistake was setting the variables inside $(document).ready(function() {}); and my reset wouldn't work.
+// set the targetNumber from 19-120
+var targetNumber = Math.floor(Math.random() * 102 + 19);
+
+// set the number-to-guess header to match the targetNumber
+// eventually this will allow us to change the HTML to match the value in JavaScript.
+$("#number-to-guess").text(targetNumber);
+
+
+// give each crystal a random # from 1-12
+var crystal1 = Math.floor(Math.random() * 12 + 1);
+var crystal2 = Math.floor(Math.random() * 12 + 1);
+var crystal3 = Math.floor(Math.random() * 12 + 1);
+var crystal4 = Math.floor(Math.random() * 12 + 1);
+
+	console.log(crystal1);
+	console.log(crystal2);
+	console.log(crystal3);
+	console.log(crystal4);
 
 // create a counter to keep track of the users total
 var counter = 0;
 var wins = 0;
 var losses = 0;
 
-var targetNumber = 0;
-var crystal1 = 0;
-var crystal2 = 0;
-var crystal3 = 0;
-var crystal4 = 0;
-
-
 // function to reset the game
-function reset () {
-// 	alert('reset');
+function reset() {
 	targetNumber = Math.floor(Math.random() * 102 + 19);
-// 	console.log("targetNumber = " + targetNumber);
- 	crystal1 = Math.floor(Math.random() * 12 + 1);
-// 	console.log("c1 = " + crystal1);
+	crystal1 = Math.floor(Math.random() * 12 + 1);
 	crystal2 = Math.floor(Math.random() * 12 + 1);
 	crystal3 = Math.floor(Math.random() * 12 + 1);
 	crystal4 = Math.floor(Math.random() * 12 + 1);
 	$("#crystalValue").empty();
 	counter = 0;
 
-	// each imageCrystal will be given a data attribute called data-crystalValue.
-	// This data attribute will be set equal to the Math.random value
-	$("#crystal1").attr("data-crystalvalue", crystal1);
-	$("#crystal2").attr("data-crystalvalue", crystal2);
-	$("#crystal3").attr("data-crystalvalue", crystal3);
-	$("#crystal4").attr("data-crystalvalue", crystal4);
-	
+	console.log(crystal1);
+	console.log(crystal2);
+	console.log(crystal3);
+	console.log(crystal4);
+
+
 	// changes targetNumber value on page
-	$("#number-to-guess").html(targetNumber);
-	$("#crystalValue").text('0');
+	$("#number-to-guess").text(targetNumber)
 }
-
-
-$(document).ready(function() {
-
-// set the targetNumber from 19-120
-targetNumber = Math.floor(Math.random() * 102 + 19);
-
-// set the number-to-guess header to match the targetNumber
-// eventually this will allow us to change the HTML to match the value in JavaScript.
-$("#number-to-guess").html(targetNumber);
-	
 //give the class ".crystal-img" to each crystal so that it can be CSSd
-$("#crystal1, #crystal2, #crystal3, #crystal4").addClass("crystal-image");
+	$("#crystal1, #crystal2, #crystal3, #crystal4").addClass("crystal-image");
 
-reset();
-	
-/* commented out the following lines of code because it got moved to the reset function. I created a reset function so that I can call the function when needed. This hopefully makes things less repetitive.
-// give each crystal a random # from 1-12
-crystal1 = Math.floor(Math.random() * 12 + 1);
-console.log(crystal1);
-crystal2 = Math.floor(Math.random() * 12 + 1);
-console.log(crystal2);
-crystal3 = Math.floor(Math.random() * 12 + 1);
-console.log(crystal3);
-crystal4 = Math.floor(Math.random() * 12 + 1);
-console.log(crystal4);
 
-$("#crystalValue").text('0');
-	
 // each imageCrystal will be given a data attribute called data-crystalValue.
 // This data attribute will be set equal to the Math.random value
 	$("#crystal1").attr("data-crystalvalue", crystal1);
 	$("#crystal2").attr("data-crystalvalue", crystal2);
 	$("#crystal3").attr("data-crystalvalue", crystal3);
 	$("#crystal4").attr("data-crystalvalue", crystal4);
-*/
-	
+
 // each crystal image (with all its classes and attributes) will get added to the page.
 	$("#crystal1").append(crystal1);
 	$("#crystal2").append(crystal2);
@@ -89,33 +69,29 @@ $("#crystal1, #crystal2, #crystal3, #crystal4").on("click", function () {
 	// since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
 	var crystalValue = ($(this).attr("data-crystalvalue"));
-	// when you store the click it becomes a string. parseInt the string so that it becomes a number and can be incremented.
-	crystalValue = parseInt(crystalValue); 
-	
-// we then add the crystalValue to the users "counter" which is a global variable
-// every click, from every crystal adds to the global counter.
+	crystalValue = parseInt(crystalValue); // when you store the click it becomes a string. parseInt the string so that it becomes a number and can be incremented.
+
+// add the crystalValue to the users "counter" which is a global variable
+// every click from every crystal adds to the global counter.
 	counter += crystalValue;
 	$("#crystalValue").text(counter);
 
 
-// if counter is equal to targetNumber then user wins, the counter resets back to 0, // win counter goes up 1 and is displayed, crystals get new value and there is new number to guess
+// if counter is equal to targetNumber then user wins, "You Win!" notification, win counter goes up 1 and is displayed, Guess and Crystal Value changes
 	if (counter === targetNumber) {
-		reset();
 		$("#w-l-notif").text("You Win!");
 		wins++;
-		$("#wins").text("Wins: " + wins); //is it weird I put this here? I tried putting it under var win but the counter is set to 0 and won't increment. same with var losses
-		
+		$("#wins").text("Wins: " + wins);
+		reset();
 	}
 
-// if counter goes over the targetNumber then user loses, the counter resets back to 0, losses counter goes up 1 and is displayed, crystals get new value and there is new number to guess
+// if counter is equal to targetNumber then user wins, "You Lose!" notification, losses counter goes up 1 and is displayed, Guess and Crystal Value changes
 	else if (counter >= targetNumber) {
-		reset();
 		$("#w-l-notif").text("You Lose!");
 		losses++;
 		$("#losses").text("Losses: " + losses);
-		
+		reset();
 	}
 
 });
-
 });
